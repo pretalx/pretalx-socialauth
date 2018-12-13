cd2979003b4fb5b2b452f079d381eb5a1bb84532
Tobias Kunze <r@rixx.de>
2018-12-13 10:42:49 +0100

    Initial commit

---
+++ pretalx_socialauth/__init__.py
@@ -0,0 +1,20 @@
+from django.apps import AppConfig
+from django.utils.translation import ugettext_lazy
+
+
+class PluginApp(AppConfig):
+    name = 'pretalx_socialauth'
+    verbose_name = 'OAuth2 for pretalx'
+
+    class PretalxPluginMeta:
+        name = ugettext_lazy('OAuth2 for pretalx')
+        author = 'Tobias Kunze'
+        description = ugettext_lazy("Use other websites, like GitHub or Twitter, to allow users to log in to pretalx.")
+        visible = True
+        version = '0.0.0'
+
+    def ready(self):
+        from . import signals  # NOQA
+
+
+default_app_config = 'pretalx_socialauth.PluginApp'
